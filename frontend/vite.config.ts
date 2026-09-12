f966ad1653b460a33ba6edcf496e51d1272e1c97
Rizki Budi <rizkibudi513@gmail.com>
2026-09-12 13:25:31 +0700

    feat(frontend): update tsconfig and vite config for absolute path mapping

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +17,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
